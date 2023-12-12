313309b499f697456b06139267e19bd62ab4859f
MartCabrera <cabreramartin88@gmail.com>
2023-12-11 21:02:50 -0300

    First commit

---
+++ Project.py
@@ -0,0 +1,12 @@
+#terminal
+#create .venv in specific folder
+#py -m venv .venv
+# .\activate
+
+# pip install request python-dotenv Flask
+#py -m pip install -U pip
+#Requirement already satisfied: pip in c:\users\user\documents\python\finalproject\.venv\lib\site-packages (23.3.1)
+#(.venv) PS C:\Users\User\Documents\Python\FinalProject> pip freeze > requirements.txt
+#create .gitiggnore
+
+#git init
---
+++ server.py
@@ -0,0 +1,37 @@
+#Flask instance
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/') #main page
+@app.route('/index')
+def index():
+    #return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/weather')
+def get_weather():
+        city = request.args.get('city')
+            # Error handling
+            # Check for empty strings or string with only spaces
+        if not bool(city.strip()): #strip the spaces
+            city = "Buenos Aires" #default value if its empty
+
+        weather_data = get_current_weather(city)
+        # Handle, cod 200 ok other cod error
+        # City is not found by API
+        if not weather_data["cod"] == 200:
+             return render_template('city-not-found.html')
+
+        return render_template(
+             "weather.html",
+             title=weather_data["name"],
+             status=weather_data["weather"][0]["description"].capitalize(),
+             temp=f"{weather_data['main']['temp']:.1f}",
+             feels_like=f"{weather_data['main']['feels_like']:.1f}"
+        )
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
