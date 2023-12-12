bc23942a7cb9e9cb186df7f89b41bf1df4a1d755
MartCabrera <cabreramartin88@gmail.com>
2023-12-11 21:21:38 -0300

    Second commit

--- Project.py
+++ Project.py
@@ -10,3 +10,33 @@
 #create .gitiggnore
 
 #git init
+
+#after all, use git and github
+
+#git add . ->adds all
+#git commit -m "first commit"
+
+#github.com create repository
+
+#push an existing repository from the command line
+#git remote add origin https://github.com/MartCabrera/python-weather.git
+#  git branch -M main
+#  git push -u origin main
+
+
+#render.com to develop
+#new web service
+
+#build command
+#pip install -U pip && pip install -r requirements.txt
+
+#start command
+#python3 server.py
+
+#advance
+#add enviroment variables
+#add API_KEY from my file
+#add enviroment varible which version of python
+#PYTHON_VERSION and check python version terminal py --version
+
+#CREATE WEB SERVICE
